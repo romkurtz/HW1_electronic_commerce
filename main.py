a82eabad1c505b977e95c05eba99f89ac7a70321
romkurtz <rom.kurtz@intel.com>
2021-04-14 17:45:43 +0300

    first LTM working but different results

--- main.py
+++ main.py
@@ -7,10 +7,38 @@ import copy
 
 PART_B_C = "PartB-C.csv"
 
+
 def LTM(graph: networkx.Graph, patients_0: List, iterations: int) -> Set:
+    for node in graph:
+        graph.nodes[node]['concern'] = calc_concern(set(graph.neighbors(node)), set(patients_0))
     total_infected = set(patients_0)
-    # TODO implement your code here
+    total_suspected = set(graph.nodes).difference(total_infected)
+
+    for t in range(iterations):
+        # step 1 : every node in total_suspected watches all of her infected neighbors and perform contagion if needed.
+        new_infected = set()
+        for node in total_suspected:
+            infected_neighbors = set(graph.neighbors(node)).intersection(total_infected)
+            weight_edges_sum = 0
+            for neighbor in infected_neighbors:
+                weight_edges_sum += graph.edges[node, neighbor]['weight']
+
+            if CONTAGION * weight_edges_sum >= 1 + graph.nodes[node]['concern']:
+                new_infected.add(node)
+
+        # step 2 : total_suspected = total_suspected \ new_infected and update total_infected
+        total_suspected = total_suspected.difference(new_infected)
+        total_infected = total_infected.union(new_infected)
+
+        # step 3 : every v in total_suspected updates her concern
+        for node in total_suspected:
+            graph.nodes[node]['concern'] = calc_concern(set(graph.neighbors(node)), total_infected)
+
     return total_infected
+
+
+def calc_concern(neighbors: set, patients: set) -> float:
+    return len(neighbors.intersection(patients)) / len(neighbors)
 
 
 def ICM(graph: networkx.Graph, patients_0: List, iterations: int) -> [Set, Set]:
@@ -57,7 +85,8 @@ def build_graph(filename: str) -> networkx.Graph:
         df = pd.read_csv(filename)
         G = networkx.Graph()
         for index, row in df.iterrows():
-            G.add_edge(row['from'], row['to'], weight=row['w'])
+            G.add_edge(int(row['from']), int(row['to']), weight=row['w'])
+
     else:
         G = networkx.read_edgelist(filename, delimiter=",")
         G.remove_node("from")
@@ -129,8 +158,12 @@ LETHALITY = .15
 if __name__ == "__main__":
     filename = "PartB-C.csv"
     G = build_graph(filename=filename)
-    CC = clustering_coefficient(G)
-    print(CC)
+    df = pd.read_csv('patients0.csv')
+    df_lst = [val[0] for val in df.values.tolist()]
+    bb = len(LTM(G, df_lst[:48], 6))
+    print(bb)
+    #CC = clustering_coefficient(G)
+    #print(CC)
     #hist = calc_degree_histogram(G)
     #plot_degree_histogram(hist)
 
