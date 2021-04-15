5ac890142f14749832eca19d22f94d4dad27bd84
romkurtz <rom.kurtz@intel.com>
2021-04-15 21:59:42 +0300

    EOD 15.4 - fill ICM and other plot funcs

--- main.py
+++ main.py
@@ -31,27 +31,74 @@ def LTM(graph: networkx.Graph, patients_0: List, iterations: int) -> Set:
 
         # step 3 : every v in total_suspected updates her concern
         for node in total_suspected:
-            graph.nodes[node]['concern'] = calc_concern(set(graph.neighbors(node)), total_infected)
+            graph.nodes[node]['concern'] = calc_concern_LTM(set(graph.neighbors(node)), total_infected)
 
         total_infected = total_infected.union(new_infected)
 
     return total_infected
 
 
-def calc_concern(neighbors: set, patients: set) -> float:
+def calc_concern_LTM(neighbors: set, patients: set) -> float:
     return len(neighbors.intersection(patients)) / len(neighbors)
 
 
 def ICM(graph: networkx.Graph, patients_0: List, iterations: int) -> [Set, Set]:
-    total_infected = set(patients_0)
     total_deceased = set()
-    # TODO implement your code here
+    deceased_t_minus_1 = set()
+    infected_t_minus_1 = set()
+    for node in graph:
+        graph.nodes[node]['concern'] = 0
+    total_infected = set(patients_0)
+    for node in total_infected:
+        if np.random.random() < LETHALITY:
+            deceased_t_minus_1.add(node)
+            total_deceased.add(node)
+            graph.remove_node(node)
+
+    total_suspected = set(graph.nodes).difference(total_infected)
+
+    for t in range(iterations):
+        new_infected = set()
+        new_deceased = set()
+        for node in total_suspected:
+            neighbors = graph.neighbors(node)
+            for neighbor in neighbors:
+                if neighbor in infected_t_minus_1:
+                    prob = min(1, CONTAGION*graph.edges[node][neighbor]['weight']*(1-graph.nodes[node]['concern']))
+                    if np.random.random() < prob:
+                        new_infected.add(node)
+
+        for node in new_infected:
+            if np.random.random() < LETHALITY:
+                new_deceased.add(node)
+                total_deceased.add(node)
+                graph.remove_node(node)
+
+        total_infected = (total_infected.union(new_infected)).difference(total_deceased)
+        total_suspected = total_suspected.difference((total_infected.union(total_deceased)))
+
+        for node in total_suspected:
+            graph.nodes[node]['concern'] = calc_concern_ICM(set(graph.neighbors(node)), infected_t_minus_1, deceased_t_minus_1)
+
+        infected_t_minus_1 = new_infected
+        deceased_t_minus_1 = new_deceased
+
     return total_infected, total_deceased
+
+
+def calc_concern_ICM(neighbors: set, patients: set, deceased: set) -> float:
+    numerator = len(neighbors.intersection(patients)) + 3*len(neighbors.intersection(deceased))
+    denominator = len(neighbors)
+    return min(numerator / denominator, 1)
+
 
 
 def plot_degree_histogram(histogram: Dict):
     width = 1
     plt.bar(histogram.keys(), histogram.values(), width, color='g')
+    plt.title('Degree histogram')
+    plt.xlabel('degree')
+    plt.ylabel('# nodes')
     plt.show()
 
 
@@ -122,17 +169,29 @@ def compute_lethality_effect(graph: networkx.Graph, t: int) -> [Dict, Dict]:
     mean_infected = {}
     for l in (.05, .15, .3, .5, .7):
         LETHALITY = l
+        total_deaths = 0
+        total_infected = 0
         for iteration in range(30):
             G = copy.deepcopy(graph)
             patients_0 = np.random.choice(list(G.nodes), size=50, replace=False, p=None)
-            # TODO implement your code here
+            infected, deceased = ICM(graph, patients_0, t)
+            total_deaths += len(deceased)
+            total_infected += len(infected)
+
+        mean_deaths[l] = total_deaths / 30
+        mean_infected[l] = total_infected / 30
 
     return mean_deaths, mean_infected
 
 
 def plot_lethality_effect(mean_deaths: Dict, mean_infected: Dict):
-    # TODO implement your code here
-    ...
+    plt.plot(mean_infected.keys(), mean_infected.values(), label='infected')
+    plt.plot(mean_deaths.keys(), mean_deaths.values(), label='death')
+    plt.legend()
+    plt.title('Lethality effect on death and infected')
+    plt.xlabel('Lethality')
+    plt.ylabel('mean')
+    plt.show()
 
 
 def choose_who_to_vaccinate(graph: networkx.Graph) -> List:
@@ -152,21 +211,29 @@ def choose_who_to_vaccinate_example(graph: networkx.Graph) -> List:
     return people_to_vaccinate
 
 
+def test_plot_2_dict():
+    d1 = {0: 20, 1: 30, 2: 40, 3: 50}
+    d2 = {0: 12, 1: 25, 2: 45, 3: 60}
+    plot_lethality_effect(d1, d2)
+
+
 "Global Hyper-parameters"
 CONTAGION = 1
 LETHALITY = .15
 
 if __name__ == "__main__":
+    test_plot_2_dict()
     filename = "PartB-C.csv"
     G = build_graph(filename=filename)
-    df = pd.read_csv('patients0.csv')
+    plot_degree_histogram(calc_degree_histogram(G))
+    """df = pd.read_csv('patients0.csv')
     df_lst = [val[0] for val in df.values.tolist()]
     bb = len(LTM(G, df_lst[:50], 6))
     print(bb)
-    #CC = clustering_coefficient(G)
-    #print(CC)
-    #hist = calc_degree_histogram(G)
-    #plot_degree_histogram(hist)
+    CC = clustering_coefficient(G)
+    print(CC)
+    hist = calc_degree_histogram(G)
+    plot_degree_histogram(hist)"""
 
 
 
