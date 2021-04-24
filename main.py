17420e184f2dd11f3ce077adedc49069d6f344ea
romkurtz <rom.kurtz@intel.com>
2021-04-24 17:22:52 +0300

    final version

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import numpy as np
 import networkx
 import copy
 
-PART_B_C = "PartB-C.csv"
-
 
 def LTM(graph: networkx.Graph, patients_0: List, iterations: int) -> Set:
     for node in graph:
@@ -43,16 +41,19 @@ def calc_concern_LTM(neighbors: set, patients: set) -> float:
 
 
 def ICM(graph: networkx.Graph, patients_0: List, iterations: int) -> [Set, Set]:
+    # step 0 - build the infected & deceased sets
     total_deceased = set()
-
     for node in graph:
         graph.nodes[node]['concern'] = 0
+        graph.nodes[node]['spreader_rate'] = 0
     total_infected = set(patients_0)
+
     for node in total_infected:
         if np.random.random() < LETHALITY:
             total_deceased.add(node)
 
     total_suspected = set(graph.nodes).difference(total_infected)
+    total_infected = total_infected.difference(total_deceased)
     prev_infected = total_infected.difference(total_deceased)
     prev_deceased = copy.deepcopy(total_deceased)
 
@@ -65,14 +66,15 @@ def ICM(graph: networkx.Graph, patients_0: List, iterations: int) -> [Set, Set]:
                 if neighbor in prev_infected:
                     prob = min(1, CONTAGION*graph.edges[node, neighbor]['weight']*(1-graph.nodes[node]['concern']))
                     if np.random.random() < prob:
+                        graph.nodes[neighbor]['spreader_rate'] += 1
                         if np.random.random() < LETHALITY:
                             new_deceased.add(node)
-                            total_deceased.add(node)
                         else:
                             new_infected.add(node)
                         break
 
         infected_for_calc_concern = total_infected.difference(total_deceased)
+        total_deceased = total_deceased.union(new_deceased)
         total_infected = (total_infected.union(new_infected)).difference(total_deceased)
         total_suspected = total_suspected.difference((total_infected.union(total_deceased)))
 
@@ -88,16 +90,26 @@ def ICM(graph: networkx.Graph, patients_0: List, iterations: int) -> [Set, Set]:
 def calc_concern_ICM(neighbors: set, patients: set, deceased: set) -> float:
     numerator = len(neighbors.intersection(patients)) + 3*len(neighbors.intersection(deceased))
     denominator = len(neighbors)
+    if denominator == 0:
+        return 1
     return min(numerator / denominator, 1)
 
 
-
 def plot_degree_histogram(histogram: Dict):
-    width = 1
-    plt.bar(histogram.keys(), histogram.values(), width, color='g')
-    plt.title('Degree histogram')
+    x_axis = list(histogram.keys())
+    x_ticks = x_axis[::10]
+    y_axis = list(histogram.values())
+    y_ticks = np.arange(0, max(y_axis), 100)
+    plt.figure(figsize=[15, 10])
+    plt.bar(x_axis, y_axis, color='g')
+    plt.ylim([min(histogram.values()), max(histogram.values())])
+    plt.xlim([min(histogram.keys()), max(histogram.keys())])
+    plt.xticks(x_ticks, rotation='vertical')
+    plt.yticks(y_ticks)
+    plt.title(f'Degree Histogram of {filename}')
     plt.xlabel('degree')
     plt.ylabel('# nodes')
+    plt.grid()
     plt.show()
 
 
@@ -106,7 +118,6 @@ def calc_degree_histogram(graph: networkx.Graph) -> Dict:
     Example:
     if histogram[1] = 10 -> 10 nodes have only 1 friend
     """
-    #return graph.degree
     hist = graph.degree
     degrees_lst = list()
     degrees_dict = dict()
@@ -120,15 +131,9 @@ def calc_degree_histogram(graph: networkx.Graph) -> Dict:
     return degrees_dict
 
 
-
-    histogram = {}
-    # TODO implement your code here
-    return histogram
-
-
 def build_graph(filename: str) -> networkx.Graph:
 
-    if filename == PART_B_C:
+    if filename == "PartB-C.csv":
         df = pd.read_csv(filename)
         G = networkx.Graph()
         for index, row in df.iterrows():
@@ -161,15 +166,14 @@ def calc_triangles(graph: networkx.Graph) -> float:
 def calc_triplets(graph: networkx.Graph) -> float:
     sum_triplets = 0
     for node in graph.nodes:
-        num_of_neigbhors = len(graph.adj[node])
-        sum_triplets += calc_choose(num_of_neigbhors, 2)
+        num_of_neighbors = len(graph.adj[node])
+        sum_triplets += calc_choose(num_of_neighbors, 2)
 
     return sum_triplets
 
 
 def calc_choose(up: int, down: int) -> float:
     return np.math.factorial(up) / (np.math.factorial(down) * np.math.factorial(up - down)) if up > 1 else 0
-
 
 
 def compute_lethality_effect(graph: networkx.Graph, t: int) -> [Dict, Dict]:
@@ -204,8 +208,9 @@ def plot_lethality_effect(mean_deaths: Dict, mean_infected: Dict):
 
 
 def choose_who_to_vaccinate(graph: networkx.Graph) -> List:
-    people_to_vaccinate = []
-    # TODO implement your code here
+    node2degree = dict(graph.degree)
+    sorted_nodes = sorted(node2degree.items(), key=lambda item: item[1], reverse=True)[:50]
+    people_to_vaccinate = [node[0] for node in sorted_nodes]
     return people_to_vaccinate
 
 
@@ -220,26 +225,19 @@ def choose_who_to_vaccinate_example(graph: networkx.Graph) -> List:
     return people_to_vaccinate
 
 
-def test_plot_2_dict():
-    d1 = {0: 20, 1: 30, 2: 40, 3: 50}
-    d2 = {0: 12, 1: 25, 2: 45, 3: 60}
-    plot_lethality_effect(d1, d2)
-
-
 "Global Hyper-parameters"
 CONTAGION = .8
 LETHALITY = .2
 
 if __name__ == "__main__":
-    # test_plot_2_dict()
     filename = "PartB-C.csv"
     G = build_graph(filename=filename)
-    # print(clustering_coefficient(G))
     df = pd.read_csv('patients0.csv', header=None)
     df_lst = [val[0] for val in df.values.tolist()]
-    # print(len(LTM(G, df_lst[:20], 6)))
-    infected, deceases = ICM(G, df_lst[:50], 6)
-    print("infected: ", len(infected), " deceases: ", len(deceases))
-
-
-
+    sum_infected, sum_deceased = 0, 0
+    for i in range(10):
+        infected, deceases = ICM(G, df_lst[:20], 4)
+        sum_infected += len(infected)
+        sum_deceased += len(deceases)
+        print("iter num: ", i)
+    print("infected: ", sum_infected / 10, " deceases: ", sum_deceased / 10)
